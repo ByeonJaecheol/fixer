c633d3c6f8786ac35da126e9b52424d9b741a931
LupineTouch <qewpit@naver.com>
2025-04-11 10:17:38 +0900

    빌드 에러때문에 test 페이지 삭제

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { supabase } from "@/utils/supabase";
-import SimpleBar from "../_components/echart/SimpleBar";
-
-export default async function TestPage() {
-  const {data,error} = await supabase.from('pc_assets').select('*').eq('serial_number','4CE104107V');
-  console.log('pc_assets test ',data);
-  return <div>{"test"}</div>;
-  // return <div><SimpleBar/></div>;
-}
