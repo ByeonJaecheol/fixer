09111b803cb6ae985fba2907219ab57eaa760f6a
LupineTouch <qewpit@naver.com>
2025-04-23 13:40:48 +0900

    HISTORY MENUS UI 변경 선택시 잘보이게

--- src/app/private/pc-history/_components/input/HistoryMenus.tsx
+++ src/app/private/pc-history/_components/input/HistoryMenus.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { ComputerDesktopIcon,PlusIcon,CpuChipIcon, XMarkIcon,ArrowUturnDownIcon, ViewColumnsIcon,GlobeAltIcon,PrinterIcon,WrenchScrewdriverIcon,BriefcaseIcon} from '@heroicons/react/24/outline';
+import { CheckIcon,  ComputerDesktopIcon,PlusIcon,CpuChipIcon, XMarkIcon,ArrowUturnDownIcon, ViewColumnsIcon,GlobeAltIcon,PrinterIcon,WrenchScrewdriverIcon,BriefcaseIcon} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
 export interface IHistoryCategory {
@@ -107,19 +107,19 @@ export default function HistoryMenus({categories}:{categories:IHistoryCategory[]
   const pathname = useSelectedLayoutSegment();
     return (
       // 반응형 모바일 뷰 대응
-        <div className="flex flex-wrap gap-x-4 ">
+        <div className="flex flex-wrap gap-x-4 px-4 ">
           {categories.map((category : IHistoryCategory) => (
             <Link
               key={category.id}
               href={category.href}
               className={`group flex-shrink-0 flex flex-col items-center p-2 md:px-4 border-b-2 border-transparent hover:border-blue-500 focus:outline-none focus:border-blue-500 transition-colors duration-200
-              ${category.value===pathname ? 'text-blue-600 font-bold' : 'text-gray-700 font-medium'}
+              ${category.value===pathname ? 'text-blue-600 font-bold ' : 'text-gray-700 font-medium'}
               `}
             >
-              <div className={`p-3 rounded-lg bg-gray-50 group-hover:bg-blue-50 transition-colors duration-200 ${category.value===pathname ? 'bg-blue-200' : 'bg-gray-50'}`}>
-                <category.icon className={`w-6 h-6 ${category.color} group-hover:text-blue-600`} />
+              <div className={`p-3 rounded-lg bg-gray-50 group-hover:bg-blue-50 transition-colors duration-200 ${category.value===pathname ? 'bg-green-300' : 'bg-gray-50'}`}>
+                {category.value===pathname ? <CheckIcon className={`w-6 h-6 group-hover:text-violet-300 `} /> : <category.icon className={`w-6 h-6 ${category.color} group-hover:text-blue-600`} />}
               </div>
-              <span className={`mt-2 text-sm group-hover:text-blue-600 ${category.value===pathname ? 'text-blue-600 font-bold text-lg ' : 'text-gray-700 font-medium'}`}>
+              <span className={`mt-2 text-sm group-hover:text-blue-600 ${category.value===pathname ? 'text-blue-600 font-bold ' : 'text-gray-700 font-medium'}`}>
                 {category.name}
               </span>
             </Link>
